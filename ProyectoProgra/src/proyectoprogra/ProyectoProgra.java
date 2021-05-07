1c2c54017db453dc90d55606c95b002901499204
Romzito <betoterrors@gmail.com>
2021-05-07 12:32:30 -0500

    sout cambiodedamaxd

--- ProyectoProgra/src/proyectoprogra/ProyectoProgra.java
+++ ProyectoProgra/src/proyectoprogra/ProyectoProgra.java
@@ -17,7 +17,7 @@ public class ProyectoProgra {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola que tal compañeros");
-        System.out.println("Cambio de rama y de dama");
+        System.out.println("Cambio de rama y de dama xd");
         
     }
     
