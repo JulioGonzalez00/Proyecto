035ee53a70355832fb7d25e2226795ec308828bd
AbyBermudez21 <AbyBermudez21@gmail.com>
2021-05-07 12:25:30 -0500

    Comentario
    hola

--- ProyectoProgra/src/proyectoprogra/ProyectoProgra.java
+++ ProyectoProgra/src/proyectoprogra/ProyectoProgra.java
@@ -16,6 +16,7 @@ public class ProyectoProgra {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola que tal compañeros");
     }
     
 }
