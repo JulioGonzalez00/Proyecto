ecdcb83c79abaea8b60593f6df44e2c2bdaa5822
JulioGonzalez00 <83717453+JulioGonzalez00@users.noreply.github.com>
2021-05-19 10:09:21 -0500

    GUI Julio

--- ProyectoProgra/src/proyectoprogra/ProyectoProgra.java
+++ ProyectoProgra/src/proyectoprogra/ProyectoProgra.java
@@ -16,8 +16,6 @@ public class ProyectoProgra {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola que tal compañeros");
-        System.out.println("Cambio de rama y de dama xd");
         
     }
     
