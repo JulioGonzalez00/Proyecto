95f51b9478d2709fb6a08bf5920eda6797f30414
JulioGonzalez00 <83717453+JulioGonzalez00@users.noreply.github.com>
2021-06-08 14:41:18 -0500

    Update ProyectoProgra.java

--- ProyectoProgra/src/proyectoprogra/ProyectoProgra.java
+++ ProyectoProgra/src/proyectoprogra/ProyectoProgra.java
@@ -6,13 +6,17 @@ public class ProyectoProgra {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         // TODO code application logic here
         PortadaHotel port = new PortadaHotel();
         PruebaInterfazGrafica login = new PruebaInterfazGrafica();
         port.setVisible(true);
         port.setLocationRelativeTo(null);
-        Thread.sleep(1000);
+        try{
+            Thread.sleep(1000);
+        }catch(InterruptedException ex){
+            System.out.println("Error al dormir el hilo");
+        }
         port.cerrar();
         //login.setVisible(true);
     }
